40038bd51c7001b1fd032416f159b44be513ca80
martin-ngigi <martinwainaina001@gmail.com>
2022-11-11 17:29:55 +0300

    Added Array

---
+++ app/src/main/java/com/example/kotlintutorials/tutorials/Arrays.kt
@@ -0,0 +1,6 @@
+package com.example.kotlintutorials.tutorials
+
+fun main(){
+    var myArray = arrayOf("Hello", "Guys", "It,s", "Martin", "Again")
+    println("Array: ${myArray[0]}") //print the first index in the array
+}
