c7df83b17796e24d91dd1610d16d787d92e324c2
martin-ngigi <martinwainaina001@gmail.com>
2022-11-11 13:57:22 +0300

    Added strings

---
+++ app/src/main/java/com/example/kotlintutorials/tutorials/Strings.kt
@@ -0,0 +1,8 @@
+package com.example.kotlintutorials.tutorials
+
+fun main(){
+
+    //String
+    var martinString = "Martin is a developer"
+    println("\nThe String is: ${martinString.uppercase()}")
+}
