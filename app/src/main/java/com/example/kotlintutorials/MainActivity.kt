c9836fd3b6a2c4640b18afa59028b39dcfad6eda
martin-ngigi <martinwainaina001@gmail.com>
2022-11-13 14:23:19 +0300

    Setting text in Textview

--- app/src/main/java/com/example/kotlintutorials/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorials/MainActivity.kt
@@ -8,19 +8,26 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+
     var TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //handle btn_apply when clicked
         btn_apply.setOnClickListener{
+            //get Text from the view
             val firstName = edt_firstname.text.toString()
             val lastName = edt_lastname.text.toString()
             val birthDate = edt_birth_date.text.toString()
             val country = edt_country.text.toString()
 
             Log.d(TAG, "onCreate: firstName: $firstName, lastName: $lastName, $birthDate: birthDate, country: $country")
+
+            //set text to the view
+            tv_results.text = "Your Results: firstName: $firstName, lastName: $lastName, $birthDate: birthDate, country: $country"
         }
     }
 }
---
+++ app/src/main/java/com/example/kotlintutorials/TIPS.kt
@@ -0,0 +1,8 @@
+package com.example.kotlintutorials
+
+class TIPS {
+    /**
+     1. Use Use Ctrl+alt+L to reformat code
+
+     */
+}
