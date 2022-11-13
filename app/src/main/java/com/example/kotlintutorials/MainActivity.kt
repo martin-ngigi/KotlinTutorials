5b853f0b485e7a5a4486fca1bbe2e472994c67e7
martin-ngigi <martinwainaina001@gmail.com>
2022-11-13 13:55:58 +0300

    Added button to get data from Views

--- app/src/main/java/com/example/kotlintutorials/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorials/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.kotlintutorials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    var TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_apply.setOnClickListener{
+            val firstName = edt_firstname.text.toString()
+            val lastName = edt_lastname.text.toString()
+            val birthDate = edt_birth_date.text.toString()
+            val country = edt_country.text.toString()
+
+            Log.d(TAG, "onCreate: firstName: $firstName, lastName: $lastName, $birthDate: birthDate, country: $country")
+        }
     }
 }
