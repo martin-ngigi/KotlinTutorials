418784fc53ade389642858cbb63a699bbf10a6ba
martin-ngigi <martinwainaina001@gmail.com>
2022-11-13 20:12:59 +0300

    moved methods  from onCreate

--- app/src/main/java/com/example/kotlintutorials/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorials/MainActivity.kt
@@ -23,16 +23,8 @@ class MainActivity : AppCompatActivity() {
 
         //handle btn_apply when clicked
         btn_apply.setOnClickListener{
-            //get Text from the view
-            val firstName = edt_firstname.text.toString()
-            val lastName = edt_lastname.text.toString()
-            val birthDate = edt_birth_date.text.toString()
-            val country = edt_country.text.toString()
 
-            Log.d(TAG, "onCreate: firstName: $firstName, lastName: $lastName, $birthDate: birthDate, country: $country")
-
-            //set text to the view
-            tv_results.text = "Your Results: firstName: $firstName, lastName: $lastName, $birthDate: birthDate, country: $country"
+            applyMethod()
         }
 
         //handle btn_add when clicked
@@ -57,69 +49,94 @@ class MainActivity : AppCompatActivity() {
 
         //handle btn_order when clicked
         btn_order.setOnClickListener {
-            //check what user has checked in the radio button group
-            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
-            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
-
-            //should return true or false
-            val cheese = cbCheese.isChecked
-            val onions = cbOnions.isChecked
-            val salad = cbSalad.isChecked
-
-            val orderString = "You ordered a bugger with:\n"+
-                    "${meat.text}\n"+
-                    (if (cheese) "\nCheese" else "")+ //if true.. answer is cheese else its empty
-                    (if (onions) "\nOnions" else "")+
-                    (if (salad) "\nSalad" else "")
-
-            //set to UI
-            tv_order.text = orderString
+            orders()
         }
 
         //handle btn_toast when clicked
         btn_toast.setOnClickListener {
-            //Toast.makeText(this, "This is my toast", Toast.LENGTH_SHORT).show()
-
-            //Custom made toast with UI...
-            Toast(this).apply {
-                duration = Toast.LENGTH_LONG
-                //"Gravity.END,Gravity.END" Will make the bar appear at the bottom... but can be changed to "0,0"
-                setGravity(Gravity.FILL_HORIZONTAL, Gravity.END,Gravity.END)
-                view = layoutInflater.inflate(R.layout.custom_toast, layout_custom_toast)
-                show()
-            }
-
+            toastCustomMessage()
         }
 
         //handle btn_openSecond when clicked
         btn_openSecond.setOnClickListener {
             //open second activity
-
-            //check if edit texts are empty
-            if (edt_full_name.text.isEmpty() || edt_age.text.isEmpty()){
-                Toast.makeText(this, "Full Name and Age cant be empty", Toast.LENGTH_SHORT).show()
-            }
-            else{
-
-                //get data from ui
-                val full_name = edt_full_name.text.toString()
-                val age = edt_age.text.toString().toInt()
-
-                //use person object to pass data
-                val person = Person(full_name, age)
-
-                //"Intent(this, SecondActivity::class.java)" will create an instance of intent
-                //"also {startActivity(it) }" will start the activity
-                Intent(this, SecondActivity::class.java).also {
-                    //put extra will set the data that is to be passed to the next activity
-                    it.putExtra("Extra_full_Name", full_name)
-                    it.putExtra("Extra_age", age)
-                    //pass data inform of object
-                    it.putExtra("Extra_Person", person)
-                    startActivity(it) //it refers to the instance of intent
-                }
-            }
-
+            openSecondActivity()
         }
+    }
+
+    private fun applyMethod() {
+        //get Text from the view
+        val firstName = edt_firstname.text.toString()
+        val lastName = edt_lastname.text.toString()
+        val birthDate = edt_birth_date.text.toString()
+        val country = edt_country.text.toString()
+
+        Log.d(TAG, "onCreate: firstName: $firstName, lastName: $lastName, $birthDate: birthDate, country: $country")
+
+        //set text to the view
+        tv_results.text = "Your Results: firstName: $firstName, lastName: $lastName, $birthDate: birthDate, country: $country"
+
+    }
+
+    private fun orders() {
+        //check what user has checked in the radio button group
+        val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
+        val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
+
+        //should return true or false
+        val cheese = cbCheese.isChecked
+        val onions = cbOnions.isChecked
+        val salad = cbSalad.isChecked
+
+        val orderString = "You ordered a bugger with:\n"+
+                "${meat.text}\n"+
+                (if (cheese) "\nCheese" else "")+ //if true.. answer is cheese else its empty
+                (if (onions) "\nOnions" else "")+
+                (if (salad) "\nSalad" else "")
+
+        //set to UI
+        tv_order.text = orderString
+    }
+
+    private fun toastCustomMessage() {
+        //Toast.makeText(this, "This is my toast", Toast.LENGTH_SHORT).show()
+
+        //Custom made toast with UI...
+        Toast(this).apply {
+            duration = Toast.LENGTH_LONG
+            //"Gravity.END,Gravity.END" Will make the bar appear at the bottom... but can be changed to "0,0"
+            setGravity(Gravity.FILL_HORIZONTAL, Gravity.END,Gravity.END)
+            view = layoutInflater.inflate(R.layout.custom_toast, layout_custom_toast)
+            show()
+        }
+
+    }
+
+    private fun openSecondActivity() {
+        //check if edit texts are empty
+        if (edt_full_name.text.isEmpty() || edt_age.text.isEmpty()){
+            Toast.makeText(this, "Full Name and Age cant be empty", Toast.LENGTH_SHORT).show()
+        }
+        else{
+
+            //get data from ui
+            val full_name = edt_full_name.text.toString()
+            val age = edt_age.text.toString().toInt()
+
+            //use person object to pass data
+            val person = Person(full_name, age)
+
+            //"Intent(this, SecondActivity::class.java)" will create an instance of intent
+            //"also {startActivity(it) }" will start the activity
+            Intent(this, SecondActivity::class.java).also {
+                //put extra will set the data that is to be passed to the next activity
+                it.putExtra("Extra_full_Name", full_name)
+                it.putExtra("Extra_age", age)
+                //pass data inform of object
+                it.putExtra("Extra_Person", person)
+                startActivity(it) //it refers to the instance of intent
+            }
+        }
+
     }
 }
