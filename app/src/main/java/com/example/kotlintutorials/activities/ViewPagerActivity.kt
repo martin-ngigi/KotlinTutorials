2e8d330eb965dd440e1eed2b7bc848379949ca7e
martin-ngigi <martinwainaina001@gmail.com>
2022-11-15 18:51:09 +0300

    Added Top Tab Navigations

--- app/src/main/java/com/example/kotlintutorials/activities/ViewPagerActivity.kt
+++ app/src/main/java/com/example/kotlintutorials/activities/ViewPagerActivity.kt
@@ -2,9 +2,12 @@ package com.example.kotlintutorials.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import com.example.kotlintutorials.R
 import com.example.kotlintutorials.adapters.AdapterViewPager
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_view_pager.*
 
 class ViewPagerActivity : AppCompatActivity() {
@@ -24,6 +27,7 @@ class ViewPagerActivity : AppCompatActivity() {
         val adapter = AdapterViewPager(images)
         viewPager.adapter = adapter
 
+        /**
         //set orientation
         viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
@@ -31,6 +35,28 @@ class ViewPagerActivity : AppCompatActivity() {
         viewPager.beginFakeDrag()
         viewPager.fakeDragBy(-10f) //"-" will move to left
         viewPager.endFakeDrag()
+        */
+
+        //Declare tab layout and set the view pager inside... but viewPager can be replaced with fragments
+        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+            tab.text = "Tab ${position+1}"
+        }.attach()
+
+        //handle tab select listener.
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@ViewPagerActivity, "Selected Tab ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@ViewPagerActivity, "UnSelected Tab ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@ViewPagerActivity, "ReSelected Tab ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
 
     }
 }
